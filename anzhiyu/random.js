a62e84f7ab48cc818c3f991fa9048d3af3121bd0
3hstudio <2409375386@qq.com>
2024-07-15 11:52:20 +0800

    Site updated: 2024-07-15 11:52:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/14/第一个作品/"];function toRandomPost(){
+var posts=["2024/07/15/first/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
