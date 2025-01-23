b3d578d1b51c55872ebd4c47b3a6bd341eb01eee
3hstudio <2409375386@qq.com>
2025-01-23 18:50:31 +0800

    Site updated: 2025-01-23 18:50:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/22/code/","2024/07/15/first/","2024/07/18/物理-1/","2024/07/13/中考快乐/","2024/08/19/middle/","2024/10/12/2024-10-12-sd/"];function toRandomPost(){
+var posts=["2024/07/22/code/","2024/07/15/first/","2024/07/18/物理-1/","2024/07/13/中考快乐/","2024/08/19/middle/","2024/10/12/2024-10-12-sd/","2025/01/23/aa/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
